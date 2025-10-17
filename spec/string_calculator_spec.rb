a9a09b436554e5bd5fa959e94ff37977dd58e4e6
Maharajan <maharajaskm@gmail.com>
2025-10-17 23:07:39 +0530

    Validation for negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,28 +12,38 @@ RSpec.describe StringCalculator do
     end
   end
   describe "Multiple string input with ',' seperator" do
-    it "should be same value" do
+    it "should be addition of given input = 6" do
       expect(instance.add("1,2,3")).to be == 6
     end
   end
   describe "Multiple string input with ';' seperator" do
-    it "should be same value" do
+    it "should be addition of given input = 6" do
       expect(instance.add("1;2;3")).to be == 6
     end
   end
   describe "Multiple string input with ';' and ',' seperator" do
-    it "should be same value" do
+    it "should be addition of given input = 10" do
       expect(instance.add("1,2;3;4")).to be == 10
     end
   end
   describe "Multiple string input with ',' seperator and '\n'" do
-    it "should be same value" do
+    it "should be addition of given input = 6" do
       expect(instance.add("\n1,2,3")).to be == 6
     end
   end
   describe "Multiple string input with ',' seperator and '\n' in single quote" do
-    it "should be same value" do
+    it "should be addition of given input = 6" do
       expect(instance.add('\n1,2,3')).to be == 6
+    end
+  end
+  describe "Multiple negative string input" do
+    it "should be a exception" do
+      expect{instance.add('-1,2,3,-4')}.to raise_error("Negative numbers not allowed <-1,-4>")
+    end
+  end
+  describe "Single negative string input" do
+    it "should be a exception" do
+      expect{instance.add('1,-2,3,4')}.to raise_error("Negative numbers not allowed <-2>")
     end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,23 @@
 class StringCalculator
   def add(str)
     return 0 if str.empty?
-    str.to_s.split(/[\\n,;:\s]/).reject{|e| e.empty?}.map{|e| e.to_i}.inject(:+)
+    arr = to_arr(str)
+    negative_arr = check_negative_numbers(arr)
+    if negative_arr.any?
+      throw_exception(negative_arr)
+    end
+    arr.inject(:+)
+  end
+
+  def to_arr(str)
+    str.to_s.split(/[\\n,;:\s]/).compact.reject{|e| e.empty?}.map{|e| e.to_i}
+  end
+
+  def check_negative_numbers(arr)
+    arr.select{|e| e.negative? }
+  end
+
+  def throw_exception(negative_arr)
+    raise "Negative numbers not allowed <#{negative_arr.join(',')}>"
   end
 end
